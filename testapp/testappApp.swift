64b73d9eca231ebb50c8d6759037e451531c0699
Roman Dudchuk <roma.dudchuk@gmail.com>
2022-10-05 22:32:02 +0200

    add project

---
+++ testapp/testappApp.swift
@@ -0,0 +1,17 @@
+//
+//  testappApp.swift
+//  testapp
+//
+//  Created by Roman Dudchuk on 05.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct testappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
