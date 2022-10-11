04a1f1e2acd659fb37e99b7013ad29d0c016ccbc
Roman Dudchuk <roma.dudchuk@gmail.com>
2022-10-11 22:56:38 +0200

    хуй

--- testapp/ContentView.swift
+++ testapp/ContentView.swift
@@ -8,47 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
-   @ObservedObject var manager = Korobka()
+    @ObservedObject var manager = Korobka()
+    @State var result = 0
+    @State var a = "0"
+    @State var b = "0"
     var body: some View {
         VStack {
+            Text("result \(result)")
             Spacer()
-            Button ("*") {
-                //action
-                manager.multiplication()
-            }
-            HStack{
-                Button ("-") {
-                    //action
-                    manager.minus()
-                }
-                Text ("\(manager.x)")
-                Button ("+") {
-                    //action
-                    manager.plus()
-                }
-            }
-            Button("/") {
-                //action
-                manager.division()
+            HStack {
+                TextField("a", text: $a)
+                TextField("b", text: $b)
             }
             Spacer()
-            HStack{
-                Button ("100") {
-                    manager.one()
+            HStack {
+                Button {
+                    result = manager.minus(a: Int(a) ?? 0, b: Int(b) ?? 0)
+                } label: {
+                    Text("-")
                 }
-                Button ("200") {
-                    manager.two()
+                .frame(width: 30.0, height: 30.0)
+                .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.black/*@END_MENU_TOKEN@*/)
+                .foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
+                .cornerRadius(/*@START_MENU_TOKEN@*/20.0/*@END_MENU_TOKEN@*/)
+                
+                Button {
+                    result = manager.plus(a: Int(a) ?? 0, b: Int(b) ?? 0)
+                } label: {
+                    Text("+")
                 }
-                Button ("300") {
-                    manager.three()
+                
+                Button {
+                    result = manager.division(a: Int(a) ?? 0, b: Int(b) ?? 0)
+                } label: {
+                    Text("/")
                 }
-            }
-            Spacer()
-            Button ("Test"){
-                manager.test()
+                
+                Button {
+                    result = manager.multiplication(a: Int(a) ?? 0, b: Int(b) ?? 0)
+                } label: {
+                    Text("*")
+                }
             }
         }
         .padding()
+        .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.green/*@END_MENU_TOKEN@*/)
     }
 }
 
--- testapp/Manager.swift
+++ testapp/Manager.swift
@@ -8,39 +8,24 @@
 import Foundation
 
 class Korobka: ObservableObject {
+   
     @Published var x = 0
-    func minus() {
-        x -= 1
-        print (x)
+    
+    func minus(a: Int, b: Int) -> Int {
+        a - b
     }
-    func plus() {
-        x += 1
-        print (x)
+    
+    func plus(a: Int, b: Int) -> Int {
+        a + b
+    }
+    
+    func multiplication(a: Int, b: Int) -> Int {
+        a * b
+    }
+    
+    func division(a: Int, b: Int) -> Int {
+        a / b
+    }
+    
 
-    }
-    func multiplication() {
-        x *= 
-        print (x)
-
-    }
-    func division() {
-        x /= 3
-        print (x)
-
-    }
-    func three() {
-        x = 300
-        print(x)
-    }
-    func one() {
-        x = 100
-    }
-    func two() {
-        x = 200
-    }
-    func test() {
-        if x % 2 == 0  {
-            x = 0
-        }
-    }
 }
